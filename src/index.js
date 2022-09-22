12ada4160c363d1ea83f6ba0cb6f5c9b67da747f
Bruno Alves <vespidhook@gmail.com>
2022-09-21 22:11:03 -0300

    listando-extrato

--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (customer) => customer.cpf === cpf
+    );
+
+    if(customerAlreadyExists) {
+        response.status(400).json({ error: "Customer already exists!" });
+    }
+    
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+    const { cpf } = request.params;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    return response.json(customer.statement);
+});
 
 app.listen(3333);
